3b6933a0fd66a4a784f5ba1d851b053f42805ed6
FelipeLRTCS <trevisolfelipe85@gmail.com>
2024-07-29 18:53:35 -0300

    umas parada feita ai

---
+++ src/chessMain/ChessGame.java
@@ -0,0 +1,13 @@
+package chessMain;
+
+import boardGame.*;
+import chess.ChessMatch;
+
+public class ChessGame{
+	public static void main(String[] args) {
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+	
+	}
+}
--- src/chessMain/chessGame.java
+++
@@ -1,8 +0,0 @@
-package chessMain;
-
-public class chessGame{
-	public static void main(String[] args) {
-		System.out.println("ai caralho que mina maluca");
-		System.out.println("Mais um teste com essa porra de git, CLI malfeita do caralho vai se foder");
-	}
-}
