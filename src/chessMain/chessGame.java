ff0dc99af5ef36901bd922e8d962c47057341689
FelipeLRTCS <trevisolfelipe85@gmail.com>
2024-07-29 16:25:59 -0300

    haha2

--- src/chessMain/chessGame.java
+++ src/chessMain/chessGame.java
@@ -3,5 +3,6 @@ package chessMain;
 public class chessGame{
 	public static void main(String[] args) {
 		System.out.println("ai caralho que mina maluca");
+		System.out.println("Mais um teste com essa porra de git, CLI malfeita do caralho vai se foder");
 	}
 }
